79a3d7848b0e835389a4c279749db52625e6c0a2
Brañ Thë Brøkèñ <abhishekraj482367@gmail.com>
2025-05-16 18:47:22 +0000

    add space between notch and phone body

--- src/components/mobile-layout.tsx
+++ src/components/mobile-layout.tsx
@@ -45,7 +45,7 @@ const MobileLayout: React.FC<MobileLayoutProps> = ({ children }) => {
       {/* Phone Bezel */}
       <div className="relative w-full max-w-[410px] aspect-[410/844] h-auto max-h-full bg-neutral-800 dark:bg-neutral-900 rounded-[60px] shadow-2xl p-3 border-4 border-neutral-700 dark:border-neutral-800">
         {/* Notch physical overlay */}
-        <div className="absolute top-2.5 left-1/2 -translate-x-1/2 w-[100px] h-6 bg-neutral-800 dark:bg-neutral-900 rounded-full z-20">
+        <div className="absolute top-3.5 left-1/2 -translate-x-1/2 w-[100px] h-6 bg-neutral-800 dark:bg-neutral-900 rounded-full z-20">
           <div className="w-10 h-1 bg-neutral-600 dark:bg-neutral-700 rounded-full mx-auto mt-2"></div>
         </div>
 
@@ -147,4 +147,3 @@ const MobileLayout: React.FC<MobileLayoutProps> = ({ children }) => {
 };
 
 export default MobileLayout;
-
